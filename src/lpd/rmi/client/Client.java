d85998489548805bc9794c286f159231f6bc8a4c
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-04-28 17:37:57 -0300

    Actualizar cliente

--- src/lpd/rmi/client/Client.java
+++ src/lpd/rmi/client/Client.java
@@ -5,12 +5,14 @@ import java.rmi.RemoteException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-import lpd.rmi.server.WeatherService;
+import lpd.rmi.data.Sign;
+import lpd.rmi.data.Info;
+import lpd.rmi.server.InfoService;
 
 public class Client {
-    
+
     private static String host = "127.0.0.1";
-    private static int port = 24001;
+    private static int port = 24000;
 
     private static void parseArgs(String args[]) {
         if (args.length < 2) {
@@ -28,20 +30,29 @@ public class Client {
 
         try {
             boolean exit = false;
-            String date = null;
-            String sign = null;
+            String dateInput = null;
+            String signInput = null;
             Scanner scanner = new Scanner(System.in);
-            String name = String.format("//%s:%s/Weather", host, port);
-            WeatherService weather = (WeatherService) Naming.lookup(name);
-            
+            String name = String.format("//%s:%s/Info", host, port);
+            InfoService infoService = (InfoService) Naming.lookup(name);
+
             do {
                 System.out.println("Escribir fecha:");
-                date = scanner.nextLine();
+                dateInput = scanner.nextLine();
                 System.out.println("Escribir signo:");
-                sign = scanner.nextLine();
-                
-                System.out.println(weather.getWeather(LocalDate.parse(date)));
-                
+                signInput = scanner.nextLine();
+
+                LocalDate date = LocalDate.parse(dateInput);
+                Sign sign = Sign.valueOf(signInput.toUpperCase());
+
+                if (date == null)
+                    System.out.println("Fecha incorrecta");
+                if (sign == null)
+                    System.out.println("Signo desconocido");
+
+                Info info = infoService.getInfo(date, sign);
+                System.out.println(info);
+
                 System.out.println("Presionar Entrar para otra consulta / N para salir");
                 switch (System.in.read()) {
                     case 'N':
@@ -49,18 +60,18 @@ public class Client {
                       exit = true;
                     break;
                 }
-                
+
             } while (!exit);
-            
+
             scanner.close();
-            
+
         } catch (RemoteException e) {
             System.err.println("Error de comunicacion: " + e.toString());
         } catch (Exception e) {
             System.err.println("Excepcion en Client:");
             e.printStackTrace();
         }
-        
+
         System.out.println("Finalizado.");
     }
 
